77c5f98be751298e1ab55bdcec0e9d5bbb760591
David Gomez <dagove47@gmail.com>
2022-10-29 03:33:38 -0600

    emailjs and form validation
    emailjs functionality and form validation added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
-
 const loader = document.getElementById("loader");
+const formNewMessage = document.getElementById('formNewMessage');
+const formInputs = document.querySelectorAll('#newName, #newEmail, #newSubject, #newMessage')
+const submitBtn = document.getElementById('submitBtn');
 
 window.addEventListener("load", () => {
     loader.style.display = "none";
 })
+
+
+formNewMessage.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    if (!formNewMessage.checkValidity()) {
+        formNewMessage.classList.add('was-validated');
+    } else {
+        submitBtn.value = 'Sending...';
+        const serviceID = 'default_service';
+        const templateID = 'portfolio_form';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                submitBtn.value = 'Send Message!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert('Sent!');
+            }, (err) => {
+                submitBtn.value = 'Send Message!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert(JSON.stringify(err));
+            });
+    }
+});
+
+function clearInputs(inputs) {
+    inputs.forEach(input => {
+        input.value = '';
+    });
+}
