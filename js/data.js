4a1c238062eb023f126cb18b971e22adb2d07e08
David Gomez <dagove47@gmail.com>
2022-10-31 14:34:02 -0600

    resume json and html templates
    resume json added as an object and html templates added

---
+++ js/data.js
@@ -0,0 +1,297 @@
+/**
+    Some data need to be updated
+**/
+export const resume = {
+    "basics": {
+        "name": "David Gómez",
+        "label": "Web Developer",
+        "image": "assets/Profile_David_Gomez.jpg",
+        "imageDescription": "Profile picture David Gomez",
+        "email": "dagove47@gmail.com",
+        "phone": "",
+        "url": "",
+        "summary": "I'm a Front-End Developer for Accenture in Costa Rica.<br>" + 
+                    "I have serious interest for UI effects, animations and creating intuitive, dynamic user experiences.<br>" +
+                    "Let's make something special.",
+        "location": {
+            "address": "1234 test St",
+            "postalCode": "SA 12345",
+            "city": "San Jose",
+            "countryCode": "CRC",
+            "region": "San Jose"
+        },
+        "profiles": [{
+                "network": "Linkedin",
+                "username": "David Gómez",
+                "url": "https://www.linkedin.com/in/david-gomez-v/",
+                "iconClassCall": "bi bi-linkedin"
+            },
+            {
+                "network": "Github",
+                "username": "David Gómez",
+                "url": "https://github.com/dagove47",
+                "iconClassCall": "bi bi-github"
+            },
+            {
+                "network": "Facebook",
+                "username": "David Gómez",
+                "url": "https://www.facebook.com/david.venegas.986/",
+                "iconClassCall": "bi bi-facebook"
+            }
+        ]
+    },
+    "work": [{
+            "name": "DollarPerHead.com",
+            "position": "Content Management",
+            "image": "assets/Logo_DollarPerHead.png",
+            "imageDescription": "DollarPerHead.com Logo",
+            "url": "https://www.dollarperhead.com/",
+            "startDate": "2013-01-01",
+            "endDate": "2014-01-01",
+            "summary": "Description…",
+            "highlights": [
+                "Started the company"
+            ]
+        },
+        {
+            "name": "Schule Wielandstraße",
+            "position": "Bundesfreiwilligendienst",
+            "image": "assets/Logo_Schule_Wielandstraße.jpg",
+            "imageDescription": "Schule Wielandstraße Logo",
+            "url": "https://schule-wielandstrasse.hamburg.de/",
+            "startDate": "2013-01-01",
+            "endDate": "2014-01-01",
+            "summary": "Description…",
+            "highlights": [
+                "Started the company"
+            ]
+        },
+        {
+            "name": "Concentrix",
+            "position": "IT Advisor",
+            "image": "assets/Logo_Concentrix.png",
+            "imageDescription": "Concentrix Logo",
+            "url": "https://www.concentrix.com/",
+            "startDate": "2013-01-01",
+            "endDate": "2014-01-01",
+            "summary": "Description…",
+            "highlights": [
+                "Started the company"
+            ]
+        },
+        {
+            "name": "Accenture",
+            "position": "Web Developer",
+            "image": "assets/Logo_Accenture.jpg",
+            "imageDescription": "Accenture Logo",
+            "url": "https://www.accenture.com/",
+            "startDate": "2013-01-01",
+            "endDate": "Present",
+            "summary": "Description…",
+            "highlights": [
+                "Started the company"
+            ]
+        }
+    ],
+    "volunteer": [{
+        "organization": "Organization",
+        "position": "Volunteer",
+        "url": "https://organization.com/",
+        "startDate": "2012-01-01",
+        "endDate": "2013-01-01",
+        "summary": "Description…",
+        "highlights": [
+            "Awarded 'Volunteer of the Month'"
+        ]
+    }],
+    "education": [{
+        "institution": "Fidélitas University",
+        "url": "https://institution.com/",
+        "area": "Computer Systems Engineering",
+        "studyType": "Bachelor",
+        "startDate": "2011-01-01",
+        "endDate": "2013-01-01",
+        "score": "",
+        "courses": [
+            "DB1101 - Basic SQL"
+        ]
+    }],
+    "awards": [{
+        "title": "Award",
+        "date": "2014-11-01",
+        "awarder": "Company",
+        "summary": "There is no spoon."
+    }],
+    "certificates": [{
+        "name": "Full Stack Software Development",
+        "date": "2021-04-30",
+        "issuer": "4Geeks Academy",
+        "url": "https://certificate.com"
+    }],
+    "publications": [{
+        "name": "Publication",
+        "publisher": "Company",
+        "releaseDate": "2014-10-01",
+        "url": "https://publication.com",
+        "summary": "Description…"
+    }],
+    "skills": [{
+        "name": "Web Development",
+        "level": "Mid-Level",
+        "keywords": [
+            "JavaScript",
+            "React",
+            "HTML5",
+            "CSS3",
+            "SASS",
+            "AEM",
+            "Java",
+            "Linux"
+        ]
+    }],
+    "languages": [{
+            "language": "English",
+            "fluency": "C1"
+        },
+        {
+            "language": "German",
+            "fluency": "B1"
+        },
+        {
+            "language": "Spanish",
+            "fluency": "Native speaker"
+        }
+    ],
+    "interests": [{
+        "name": "Wildlife",
+        "keywords": [
+            "Ferrets",
+            "Unicorns"
+        ]
+    }],
+    "references": [{
+        "name": "Jane Doe",
+        "reference": "Reference…"
+    }],
+    "projects": [{
+            "name": "Sorting Algorithms Visualizer",
+            "description": "JavaScript",
+            "image": "assets/Project_1.png",
+            "imageDescription": "Sorting Algorithms Visualizer project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/Sorting_Algorithms_Visualizer",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        },
+        {
+            "name": "Contacts App",
+            "description": "JavaScript",
+            "image": "assets/Project_2.png",
+            "imageDescription": "Contacts App project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/Contacts_App",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        },
+        {
+            "name": "Rick and Morty API",
+            "description": "JavaScript",
+            "image": "assets/Project_3.png",
+            "imageDescription": "Rick and Morty API project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/Rick_and_Morty_API",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        },
+        {
+            "name": "BeGamer",
+            "description": "React, JavaScript, Python, Flask, PostgreSQL",
+            "image": "assets/Project_4.png",
+            "imageDescription": "BeGamer project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/4Geeks-Final-Project",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        },
+        {
+            "name": "Tic Tac Toe",
+            "description": "React, JavaScript",
+            "image": "assets/Project_5.png",
+            "imageDescription": "Tic Tac Toe project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/tic-tac-toe-with-react",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        },
+        {
+            "name": "To do list",
+            "description": "React, JavaScript",
+            "image": "assets/Project_6.png",
+            "imageDescription": "To do list project image",
+            "highlights": [
+                "Won award at AIHacks 2016"
+            ],
+            "keywords": [
+                "HTML"
+            ],
+            "startDate": "2019-01-01",
+            "endDate": "2021-01-01",
+            "url": "https://github.com/dagove47/todo-list-application-using-react",
+            "roles": [
+                "Team Lead"
+            ],
+            "entity": "Entity",
+            "type": "application"
+        }
+    ]
+};
--- js/main.js
+++ js/main.js
@@ -1,12 +1,105 @@
+// import * as resume from './data.js';
+import {
+    resume
+} from "./data.js";
+
 const loader = document.getElementById("loader");
 const formNewMessage = document.getElementById('formNewMessage');
 const formInputs = document.querySelectorAll('#newName, #newEmail, #newSubject, #newMessage')
 const submitBtn = document.getElementById('submitBtn');
 
+const as = document.getElementById('');
+
+
 window.addEventListener("load", () => {
     loader.style.display = "none";
 })
 
+/* 
+    About
+*/
+const aboutInfo = document.querySelector('[data-aboutInfo]');
+
+const aboutInfoText = document.createElement('p');
+aboutInfoText.innerHTML = resume.basics.summary;
+aboutInfo.append(aboutInfoText);
+
+const skills = document.createElement('ul');
+skills.className = "skills";
+const skillList = resume.skills[0].keywords;
+skillList.map(skill => {
+    const liSkill = document.createElement('li');
+    liSkill.textContent = skill;
+    skills.append(liSkill);
+})
+aboutInfo.append(skills);
+
+const aboutInfoImg = document.querySelector('[data-aboutInfo-img]');
+const profileImg = document.createElement('img');
+profileImg.setAttribute("src", `${resume.basics.image}`);
+profileImg.setAttribute("alt", `${resume.basics.imageDescription}`);
+aboutInfoImg.append(profileImg);
+
+
+const aboutInfoJobs = document.querySelector('[data-jobs]');
+const jobTemplate = document.querySelector('[data-jobs-template]');
+const jobs = resume.work;
+jobs.map(job => {
+    const liJob = jobTemplate.content.cloneNode(true).children[0];
+    const imgJob = liJob.querySelector('[data-job-img]');
+    const linkJob = liJob.querySelector('[data-job-link]');
+
+    imgJob.src = job.image;
+    imgJob.alt = job.imageDescription;
+    linkJob.href = job.url;
+    linkJob.textContent = job.name;
+
+    aboutInfoJobs.append(liJob);
+})
+
+/* 
+    Work
+*/
+
+const cardContainer = document.querySelector('[data-card-container]');
+const cardTemplate = document.querySelector('[data-card-template]');
+const cards = resume.projects;
+cards.map(card => {
+    const project = cardTemplate.content.cloneNode(true).children[0];
+    const cardImg = project.querySelector('[data-card-img]');
+    const cardTitle = project.querySelector('[data-card-title]');
+    const cardDescription = project.querySelector('[data-card-description]');
+    const cardLink = project.querySelector('[data-card-link]');
+
+    cardImg.src = card.image;
+    cardImg.alt = card.imageDescription;
+    cardTitle.textContent = card.name;
+    cardDescription.textContent = card.description;
+    cardLink.href = card.url;
+
+    cardContainer.append(project);
+})
+
+/* 
+    Footer
+*/
+
+const socialMediaFooter = document.querySelector('[data-footer-socialMedia]');
+const socialMediaTemplate = document.querySelector('[data-socialMedia-template]');
+const socialMedias = resume.basics.profiles;
+socialMedias.map(socialMedia => {
+    const conctact =  socialMediaTemplate.content.cloneNode(true).children[0];
+    const socialMediaIcon = conctact.querySelector('[data-socialMedia-icon]');
+
+    conctact.href = socialMedia.url;
+    socialMediaIcon.className = socialMedia.iconClassCall;
+
+    socialMediaFooter.append(conctact);
+})
+
+/* 
+    Contact Form
+*/
 
 formNewMessage.addEventListener('submit', function (event) {
     event.preventDefault();
